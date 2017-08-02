45405011bbcf65e8afc524e7038681f7efbdf1a8
Jesse <jesse.bridge@my.jcu.edu.au>
2017-08-02 16:18:47 +1000

    added 2 extra programs

---
+++ CodeInFigures/comments.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class comments {
+    /* Program comments are non-executing statements you add to a file for the purpose of the document */
+    // Program comments are non-executing statements you add to a file for the purpose of the document
+    /** Program comments are non-executing statements you add to a file for the purpose of the document **/
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Program comments are non-executing statements you add to a file for the purpose of the document");
+
+    }
+}
